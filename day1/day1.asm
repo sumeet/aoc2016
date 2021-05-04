13218315538460df52d1ad97ba58f96feb1e96e6
Sumeet Agarwal <sumeet.a@gmail.com>
2021-05-03 21:35:04 -0700

    aww yeah

--- day1/day1.asm
+++ day1/day1.asm
@@ -21,6 +21,11 @@ extern printf
 ; ...
 ; basically this means rem 4, r8 => positive values for north to west above
 
+%define N 0
+%define E 1
+%define S 2
+%define W 3
+
 
 ; accumulators:
 %define east_reg r9 ; i64: total moved east (can be negative)
@@ -32,12 +37,16 @@ extern printf
 %define str_index_reg r11 ; u64: position in parsed string
 %define init_str_index() mov str_index_reg, 0
 
+%define number_parse_reg r12 ; u64: built up parsed number
+%define init_number_parse_reg() mov number_parse_reg, 0
+
 ; misc:
-%define op_reg r12 ; for operations
+%define op_reg r13 ; for operations
+%define op_regb r13b
 %define init_op_reg() mov op_reg, 0
 
 ; data string to run on, can either be sample or input
-%define run_input sample
+%define run_input input
 %define this_char [run_input + str_index_reg]
 
 ;;;;;;;;;;;;;;;
@@ -67,9 +76,9 @@ extern printf
 ; sets flags (true or false)
   cmp %1, %2 ; val < lower bound
   jl %%false
-  cmp %1, %3 ; val < upper bound
-  jl %%true
-  jge %%false
+  cmp %1, %3 ; val <= upper bound
+  jle %%true
+  jg %%false
   %%true:
     mov op_reg, 0
     cmp op_reg, 0
@@ -81,8 +90,27 @@ extern printf
 
 %endmacro
 
-parse_digit:
-  nop
+handle_digit: ; uses arg from rdi
+  cmp dir_reg, N
+  je north
+  cmp dir_reg, E
+  je east
+  cmp dir_reg, S
+  je south
+  cmp dir_reg, W
+  je west
+  hlt
+north:
+  add north_reg, rdi
+  ret
+east:
+  add east_reg, rdi
+  ret
+south:
+  sub north_reg, rdi
+  ret
+west:
+  sub east_reg, rdi
   ret
 turn_right:
   inc dir_reg
@@ -92,6 +120,7 @@ turn_left:
   dec dir_reg
   rem 4, dir_reg
   ret
+
 main:
 init:
   init_dir_reg()
@@ -99,11 +128,27 @@ init:
   init_north_reg()
   init_str_index()
   init_op_reg()
+  init_number_parse_reg()
 parse_loop:
   cmp_between byte this_char, '0', '9'
-  push continue_parse_loop
-  je parse_digit
-  add rsp, 8
+  je is_a_number
+  jne not_a_number
+is_a_number:
+  mov op_reg, 0
+  mov op_regb, byte this_char
+  sub op_regb, '0'
+  mov rax, number_parse_reg
+  mov rdx, 10
+  mul rdx
+  mov number_parse_reg, rax
+  add number_parse_reg, op_reg
+  jmp continue_parse_loop
+end_of_number:
+  mov rdi, number_parse_reg
+  call handle_digit
+  mov number_parse_reg, 0
+  ret
+not_a_number:
   cmp byte this_char, 'R'
   push continue_parse_loop
   je turn_right
@@ -112,19 +157,28 @@ parse_loop:
   push continue_parse_loop
   je turn_left
   add rsp, 8
-  ; TODO: ' ' and 0 will both pop off the ints
-  cmp byte this_char, ' ' ; ignore spaces
-  je continue_parse_loop
+  ; ' ' means end of command
+  cmp byte this_char, ' '
+  je end_of_single_command
+  ; null terminator means end of proggy, handle this int first and terminate
   cmp byte this_char, 0 ; end of string
-  je done
+  je end_of_all_commands
 continue_parse_loop:
   inc str_index_reg
   jmp parse_loop
+end_of_single_command:
+  call end_of_number
+  jmp continue_parse_loop
+end_of_all_commands:
+  call end_of_number
 done:
-  mov rsi, dir_reg
-  ;inc str_index_reg
-  ;inc str_index_reg
-  ;movzx rsi, byte [run_input + str_index_reg]
+  abs north_reg
+  abs east_reg
+  mov rax, 0
+  add rax, north_reg
+  add rax, east_reg
+print:
+  mov rsi, rax
   mov rdi, message
   mov rax, 0
   call printf
@@ -134,5 +188,5 @@ section .data
 FALSE equ 0
 TRUE equ -1
 message db "%d", 10, 0
-sample db "R5, L5, R5, R3", 0
+;sample db "R156", 0
 input db "L3, R2, L5, R1, L1, L2, L2, R1, R5, R1, L1, L2, R2, R4, L4, L3, L3, R5, L1, R3, L5, L2, R4, L5, R4, R2, L2, L1, R1, L3, L3, R2, R1, L4, L1, L1, R4, R5, R1, L2, L1, R188, R4, L3, R54, L4, R4, R74, R2, L4, R185, R1, R3, R5, L2, L3, R1, L1, L3, R3, R2, L3, L4, R1, L3, L5, L2, R2, L1, R2, R1, L4, R5, R4, L5, L5, L4, R5, R4, L5, L3, R4, R1, L5, L4, L3, R5, L5, L2, L4, R4, R4, R2, L1, L3, L2, R5, R4, L5, R1, R2, R5, L2, R4, R5, L2, L3, R3, L4, R3, L2, R1, R4, L5, R1, L5, L3, R4, L2, L2, L5, L5, R5, R2, L5, R1, L3, L2, L2, R3, L3, L4, R2, R3, L1, R2, L5, L3, R4, L4, R4, R3, L3, R1, L3, R5, L5, R1, R5, R3, L1", 0
