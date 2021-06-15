46fcbd6d022afff9cba3c50ef933eb59b1857447
Sumeet Agarwal <sumeet.a@gmail.com>
2021-06-15 16:33:13 -0400

    wip

--- day8/day8.asm
+++ day8/day8.asm
@@ -1,18 +1,16 @@
 TOTAL_WIDTH equ 50
-
 TOTAL_HEIGHT equ 6
 OFF equ 0
 ON equ 1
 
 section .data
-
 printf_msg: db "%d", 10, 0
 input: incbin "./input"
 db 10 ; add a newline after the input
 db 0 ; null terminate the input
 
-screen_a: times 0 db TOTAL_HEIGHT * TOTAL_WIDTH
-screen_b: times 0 db TOTAL_HEIGHT * TOTAL_WIDTH
+screen_a: times 0 db TOTAL_WIDTH * TOTAL_HEIGHT
+screen_b: times 0 db TOTAL_WIDTH * TOTAL_HEIGHT
 
 this_screen: dq screen_a
 next_screen: dq screen_b
@@ -31,6 +29,35 @@ next_screen: dq screen_b
   pop rax
 %endmacro
 
+%macro match_exact_str 2
+; %1: input cursor (addr)
+; %2: string literal
+    %assign i 1
+    %strlen len %2
+    %rep len
+        %substr this_char %2 i
+
+        mov rcx, i
+        mov rbx, this_char
+
+        mov eax, %1
+        cmp byte [rax + i - 1], this_char
+        jne %%false
+        
+        %assign i i+1
+    %endrep
+    %%true:
+        add %1, len
+    
+        mov rax, 1
+        cmp rax, 1
+        jmp %%end
+    %%false:
+        mov rax, 0
+        cmp rax, 1
+    %%end:
+%endmacro
+
 section .text
 ;; TODO: without libc
 global main
@@ -39,7 +66,29 @@ extern printf
 main:
   %define input_cursor dword [rbp - 4]
   enter 4, 0
-  mov input_cursor, 0
+  mov input_cursor, input ; first character
+  input_loop:    
+    mov eax, input_cursor
+    cmp byte [rax], 0
+    je end_input_loop
+    
+    .rect:
+      match_exact_str input_cursor, "rect "
+      jne .not_rect
+      consume_number input_cursor, r10
+      jne .not_rect
+      match_exact_str input_cursor, "x"
+      jne .not_rect
+      consume_number input_cursor, r11
+      jne .not_rect
+
+    .not_rect:
+    
+    
+  continue_input_loop:
+    inc input_cursor
+  end_input_loop:
+  %undef input_cursor
   leave
 
 
