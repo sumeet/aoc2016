ebf71c54e8aa02de544a947517ba84c4186858fb
Sumeet Agarwal <sumeet.a@gmail.com>
2021-06-15 17:23:51 -0700

    wip

--- day8/day8.asm
+++ day8/day8.asm
@@ -29,9 +29,31 @@ next_screen: dq screen_b
   pop rax
 %endmacro
 
+%macro cmp_between 3
+; %1: value
+; %2: lower bound (inclusive)
+; %3: upper bound (inclusive)
+; sets flags (true or false)
+  cmp %1, %2 ; val < lower bound
+  jl %%false
+  cmp %1, %3 ; val <= upper bound
+  jle %%true
+  jmp %%false
+  %%true:
+    mov rax, 0
+    cmp rax, 0
+    jmp %%end
+  %%false:
+    mov rax, 1
+    cmp rax, 0
+  %%end:
+%endmacro
+
 %macro match_exact_str 2
 ; %1: input cursor (addr)
 ; %2: string literal
+; can use je if string matches otherwise jne
+; clobbers: rax, rbx, rcx
     %assign i 1
     %strlen len %2
     %rep len
@@ -40,7 +62,7 @@ next_screen: dq screen_b
         mov rcx, i
         mov rbx, this_char
 
-        mov eax, %1
+        mov rax, %1
         cmp byte [rax + i - 1], this_char
         jne %%false
         
@@ -58,17 +80,56 @@ next_screen: dq screen_b
     %%end:
 %endmacro
 
+%macro times_ten 1
+; %1: register
+  lea %1, [%1 + %1 + %1 + %1 + %1 + %1 + %1 + %1 + %1 + %1]
+%endmacro
+
+%macro consume_number 2
+; %1: input cursor (addr)
+; %2: into register
+; can use je if number actually consumed otherwise jne
+; clobbers: rax, rbx
+%define input_cursor_reg rbx
+  mov input_cursor_reg, %1
+
+  ; first iteration
+  cmp_between byte [input_cursor_reg], '0', '9'
+  jne %%false
+  
+  movzx rax, byte [input_cursor_reg]
+  lea %2, [rax  - '0']
+  inc byte [input_cursor_reg]
+
+  %%loop:
+    cmp_between byte [input_cursor_reg], '0', '9'
+    jne %%true ; can now bail with true because we would've already consumed a single number
+
+    movzx rax, 
+  %%true:
+    mov %1, input_cursor_reg ; save the new input cursor, consuming the number
+
+    mov rax, 1
+    cmp rax, 1
+    jmp %%end
+  %%false:
+    mov rax, 0
+    cmp rax, 1
+  %%end:
+%undef input_cursor_reg
+%endmacro
+
 section .text
 ;; TODO: without libc
 global main
 extern printf
 
 main:
-  %define input_cursor dword [rbp - 4]
-  enter 4, 0
+  %define input_cursor qword [rbp - 8]
+  enter 8, 0
   mov input_cursor, input ; first character
   input_loop:    
-    mov eax, input_cursor
+    mov rax, input_cursor
     cmp byte [rax], 0
     je end_input_loop
     
