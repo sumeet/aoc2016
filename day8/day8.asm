13560bf5bb136ea0699e01269ae5a8be510569f6
Sumeet Agarwal <sumeet.a@gmail.com>
2021-06-21 16:44:45 -0700

    finish day8a

--- day8/day8.asm
+++ day8/day8.asm
@@ -88,12 +88,12 @@ next_screen: dq screen_b
         mov rax, %1
         cmp byte [rax + i - 1], this_char
         jne %%false
-        
+
         %assign i i+1
     %endrep
     %%true:
         add %1, len
-    
+
         mov rax, 1
         cmp rax, 1
         jmp %%end
@@ -121,7 +121,7 @@ next_screen: dq screen_b
   ; first iteration
   cmp_between byte [input_cursor_reg], '0', '9'
   jne %%false
-  
+
   movzx rax, byte [input_cursor_reg]
   lea %2, [rax  - '0']
   inc input_cursor_reg
@@ -157,7 +157,7 @@ main:
   %define input_cursor qword [rbp - 8]
   enter 8, 0
   mov input_cursor, input ; first character
-  input_loop:    
+  input_loop:
     mov rax, input_cursor
     cmp byte [rax], 0
     je end_input_loop
@@ -192,7 +192,7 @@ main:
           dec y
           cmp y, 0
           jg .inner_loop
-        
+
         dec x
         cmp x, 0
         jg .outer_loop
@@ -214,62 +214,6 @@ main:
       copy_this_screen_to_next_screen
 
       %define y r10
-      %define shift_len r11
-      %define row_i r12
-      mov row_i, 0
-      .rotate_loop:
-        mov rax, y
-        mov rdx, TOTAL_WIDTH
-        mul rdx
-        add rax, row_i
-        add rax, [this_screen]
-
-        ; rbx contains the source pixel
-        movzx rbx, byte [rax]
-
-        ; find the offset of the the dest pixel is being moved to
-        mov rax, shift_len
-        add rax, row_i
-
-        mov rdx, 0
-        mov rcx, TOTAL_WIDTH
-        div rcx
-        ; new offset is in the remainder, rdx
-
-        %define new_offset rcx
-        mov new_offset, rdx
-
-        mov rax, y
-        mov rdx, TOTAL_WIDTH
-        mul rdx
-        add rax, new_offset
-        add rax, [next_screen]
-        %undef new_offset
-
-        mov byte [rax], bl
-
-        inc row_i
-        cmp row_i, TOTAL_WIDTH
-        jl .rotate_loop
-      swap_screens
-      %undef row_i
-      %undef y
-      %undef shift_len
-    .not_rotate_row:
-
-    .rotate_column:
-      match_exact_str input_cursor, "rotate column x="
-      jne .not_rotate_column
-      consume_number input_cursor, r10
-      jne .not_rotate_column
-      match_exact_str input_cursor, " by "
-      jne .not_rotate_column
-      consume_number input_cursor, r11
-      jne .not_rotate_column
-
-      copy_this_screen_to_next_screen
-
-      %define x r10
       %define shift_len r11
       %define column_i r12
       mov column_i, 0
@@ -305,15 +249,70 @@ main:
         mov byte [rax], bl
 
         inc column_i
-        cmp column_i, TOTAL_HEIGHT
+        cmp column_i, TOTAL_WIDTH
         jl .rotate_loop
       swap_screens
       %undef column_i
       %undef y
       %undef shift_len
+    .not_rotate_row:
+
+    .rotate_column:
+      match_exact_str input_cursor, "rotate column x="
+      jne .not_rotate_column
+      consume_number input_cursor, r10
+      jne .not_rotate_column
+      match_exact_str input_cursor, " by "
+      jne .not_rotate_column
+      consume_number input_cursor, r11
+      jne .not_rotate_column
+
+      copy_this_screen_to_next_screen
+
+      %define x r10
+      %define shift_len r11
+      %define row_i r12
+      mov row_i, 0
+      .rotate_col_loop:
+        mov rax, row_i
+        mov rdx, TOTAL_WIDTH
+        mul rdx
+        add rax, x
+        add rax, [this_screen]
+
+        ; rbx contains the source pixel
+        movzx rbx, byte [rax]
+
+        ; find the destination row: (row_i + shift_len) % TOTAL_HEIGHT
+        mov rax, row_i
+        add rax, shift_len
+
+        mov rdx, 0
+        mov rcx, TOTAL_HEIGHT
+        div rcx
+
+        %define dest_row rcx
+        mov dest_row, rdx
+
+        mov rax, dest_row
+        mov rdx, TOTAL_WIDTH
+        mul rdx
+        add rax, x
+        add rax, [next_screen]
+        %undef dest_row
+
+        mov byte [rax], bl
+
+        inc row_i
+        cmp row_i, TOTAL_HEIGHT
+        jl .rotate_col_loop
+      swap_screens
+      %undef row_i
+      %undef y
+      %undef shift_len
     .not_rotate_column:
-    
-    
+
+
   continue_input_loop:
     inc input_cursor
     jmp input_loop
@@ -321,10 +320,25 @@ main:
   %undef input_cursor
   leave
 
+count_on_pixels:
+  %define on_pixels_acc r10
+  %define on_pixels_acc_b r10b
+  %define i r11
+  mov on_pixels_acc, 0
+  mov i, 0
+  .count_loop:
+    mov rax, [this_screen]
+    add rax, i
+    add on_pixels_acc_b, byte [rax]
+    inc i
+    cmp i, (TOTAL_WIDTH*TOTAL_HEIGHT)
+    jl .count_loop
+  %undef i
 
 print:
   mov rdi, printf_msg
-  mov rsi, 234
+  mov rsi, on_pixels_acc
+  %undef on_pixels_acc
   mov rax, 0
   call printf
   mov rax, 0
