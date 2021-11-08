8a191f26ba40d1e31dc192dc21b9cce95a564d31
Sumeet Agarwal <sumeet.a@gmail.com>
2021-11-08 15:39:03 -0800

    wip

--- day11/src/main/scala/Main.scala
+++ day11/src/main/scala/Main.scala
@@ -11,8 +11,8 @@ def matches(gen: Item.Generator, chip: Item.Microchip): Boolean = {
   gen.name == chip.name
 }
 
-def parse(input: String): List[List[Item]] = {
-  input.linesIterator
+def parse(input: String): Facility = {
+  val floors = input.linesIterator
     .map(line => {
       var stuff = line.split(" contains ", 2)(1)
       stuff = stuff.replace("and ", "")
@@ -32,18 +32,86 @@ def parse(input: String): List[List[Item]] = {
       }
     })
     .toList
+  Facility(
+    numMoves = 0,
+    floors,
+    currentFloor = 0,
+    elevatorContents = (None, None)
+  )
+}
+
+case class Facility(
+    numMoves: Int,
+    floors: List[List[Item]],
+    currentFloor: Int,
+    elevatorContents: (Option[Item], Option[Item])
+) {
+  def isDone: Boolean = floors.init.forall(_.isEmpty) && floors.last.nonEmpty
+  def isValid: Boolean = {
+    val allItems = currentFloorContents ++ LazyList(
+      elevatorContents._1,
+      elevatorContents._2
+    ).flatten
+    val isUnconnectedChip = allItems.exists {
+      case Item.Generator(_) => false
+      case Item.Microchip(chipName) =>
+        !allItems.exists {
+          case Item.Generator(genName) if chipName == genName => true
+          case _                                              => false
+        }
+    }
+    val isAtLeastOneGenerator = allItems.exists {
+      case Item.Generator(_) => true
+      case _                 => false
+    }
+    // if a chip is ever left in the same area as another RTG, and it's not connected to its own RTG,
+    // the chip will be fried
+    val isInvalid = isUnconnectedChip && isAtLeastOneGenerator
+    !isInvalid
+  }
+  def nextMoves: List[Facility] = {
+    var allMoves = List.empty[Facility]
+
+    var elevatorSwaps = List.empty[Facility]
+    // 1. try taking stuff off the elevator, either both, or one each
+    (elevatorContents: @unchecked) match {
+      case (Some(a), Some(b)) =>
+        elevatorSwaps = elevatorSwaps ++ List(
+          copy(
+            elevatorContents = (None, None),
+            floors =
+              floors.updated(currentFloor, currentFloorContents ++ List(a, b))
+          )
+        )
+    }
+    (elevatorContents: @unchecked) match {
+      case (Some(a), _) =>
+        elevatorSwaps = elevatorSwaps ++ List(
+          copy(
+            elevatorContents = (None, elevatorContents._2),
+            floors =
+              floors.updated(currentFloor, currentFloorContents ++ List(a))
+          )
+        )
+    }
+    (elevatorContents: @unchecked) match {
+      case (_, Some(b)) =>
+        elevatorSwaps = elevatorSwaps ++ List(
+          copy(
+            elevatorContents = (elevatorContents._1, None),
+            floors =
+              floors.updated(currentFloor, currentFloorContents ++ List(b))
+          )
+        )
+    }
+    // 2. could also take any items and move them onto the elevators
+
+    allMoves.distinct.filter(_.isValid)
+  }
+  private def currentFloorContents = floors(currentFloor)
 }
 
 object Main extends App {
-  val destFloor = 3
-  val floors = parse(INPUT)
-  var currentFloor = 0
-  var numMoves = 0
-
-  def isDone: Boolean = floors.init.forall(_.isEmpty) && floors.last.nonEmpty
-
-  while (!isDone) {}
-
-  println(floors)
-  println(isDone)
+  var facility = parse(INPUT)
+  println(facility.isValid)
 }
